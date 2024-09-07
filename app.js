3de5bbab4949bdb78a6b3521a2a65e5078970478
David Payet <davidpayet570@gmail.com>
2024-09-07 18:23:05 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const hero = document.querySelector('.hero')
+const chessPiecesContainer = document.querySelector('.chessPiecesContainer')
+const nextBtn = document.querySelector('.next')
+const prevBtn = document.querySelector('.prev')
+const navToggler = document.querySelector('.navToggler')
+const navigation = document.querySelector('nav')
+
+navToggler.addEventListener('click', toggleNav)
+
+function toggleNav() {
+  navToggler.classList.toggle('active')
+  navigation.classList.toggle('active')
+}
+
+const getPieces = () => {
+  fetch('/data/pieces.json')
+    .then((res) => res.json())
+    .then((pieces) => {
+      pieces.forEach((piece) => {
+        let onePiece = document.createElement('div')
+
+        onePiece.className = 'piece'
+        onePiece.style.backgroundImage = `url(${piece.bgUrl})`
+        onePiece.innerHTML = `
+          <div class="content">
+            <p class="name">${piece.name}</p>
+            <p class="description">${piece.description}</p>
+          </div>
+        `
+
+        chessPiecesContainer.appendChild(onePiece)
+      })
+      document.body.appendChild(chessPiecesContainer)
+    })
+    .catch((error) =>
+      console.error('Erreur lors du chargement des pièces: ', error)
+    )
+}
+getPieces()
+
+nextBtn.onclick = function () {
+  showSlider('next')
+}
+
+prevBtn.onclick = function () {
+  showSlider('prev')
+}
+
+let autoNextSlide = setTimeout(() => {
+  nextBtn.click()
+}, 7000)
+
+function showSlider(type) {
+  const sliderPieces = chessPiecesContainer.querySelectorAll('.piece')
+
+  if (type === 'next') {
+    chessPiecesContainer.appendChild(sliderPieces[0])
+    hero.classList.add('next')
+  } else {
+    chessPiecesContainer.prepend(sliderPieces[sliderPieces.length - 1])
+    hero.classList.add('prev')
+  }
+
+  clearTimeout(autoNextSlide)
+
+  autoNextSlide = setTimeout(() => {
+    nextBtn.click()
+  }, 7000)
+}
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.isIntersecting && entry.target.classList.add('visible')
+    })
+  },
+  { threshold: 0.4 }
+)
+
+document
+  .querySelectorAll('.aboutContainer p')
+  .forEach((p) => observer.observe(p))
